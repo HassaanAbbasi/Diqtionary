1305fd47ce5ecfbc83b8956455fc81a91c01cb0d
Hassaan Abbasi <hassaan.abbasi@ryerson.ca>
2019-12-20 19:06:48 -0500

    Updated defReturn() in main.py to account for words not defined in data.json. Also added user input.

--- Program/main.py
+++ Program/main.py
@@ -2,10 +2,17 @@ import json
 data = json.load(open("Program/defs.json"))
 
 def defReturn(word):
-    allDefinition = data[word]
-    enum = enumerate(allDefinition,1)
-    print(word.title())
-    for definition in enum:
-        print(f"{definition[0]} - {definition[1]}")
+    word = word.lower()
+    
+    if word in data:
+        allDefinition = data[word]
+        enum = enumerate(allDefinition,1)
 
-defReturn("hot")
+        print(word.title())
+        for definition in enum:
+            print(f"{definition[0]} - {definition[1]}")
+    else:
+        print("That word doesn't exist in our dictionary. Sorry!")
+
+word = input("Welcome to Diqtionary!\nEnter a word please: ") 
+defReturn(word)
