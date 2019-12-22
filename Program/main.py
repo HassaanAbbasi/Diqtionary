66a3e67b1d41b9ac08080f57a1273a9d6f385c1c
Hassaan Abbasi <hassaan.abbasi@ryerson.ca>
2019-12-22 18:25:57 -0500

    Changed from grid() management to pack() management for a cleaner interface. Added a scrolling textbox to house definitions. Modified first case of user input.

--- Program/main.py
+++ Program/main.py
@@ -3,21 +3,27 @@ from difflib import get_close_matches
 import tkinter
 import tkinter.font as tkFont
 from tkinter import messagebox
+from tkinter import scrolledtext
 
 data = json.load(open("Program/defs.json"))
 
 #Prints the defintions of a given word
-def definition(word):
-    word = word.lower()
-
+def definition():
+    word = e1.get().lower()
+    text.config(state = "normal")
+    text.delete(1.0, tkinter.END)
+    
     #Case where the word is correctly spelled and in data
     if (word in data):
         allDefinition = data[word]
         enum = enumerate(allDefinition,1)
 
-        print(word.title())
+        temp1 = word.title()
+        text.insert(tkinter.END, temp1)
         for definition in enum:
-            print(f"{definition[0]} - {definition[1]}")
+            temp2 = f"\n{definition[0]} - {definition[1]}"
+            text.insert(tkinter.END, temp2)
+        text.config(state = "disabled")
 
     #Case where the word is a proper noun (Manchester, Paris, etc.)
     elif (word.title() in data):
@@ -77,17 +83,17 @@ messagebox.showinfo('Welcome!', "Welcome to Diqtionary! Enter a word to get it's
 prompt = tkFont.Font(family="Helvetica", size = 12, weight = "bold", underline = 1)
 
 #Prompting user for word
-tkinter.Label(window, text="Enter a word please:", font = prompt).grid(row = 0, column = 0, padx = 6)
+tkinter.Label(window, text="Enter a word please:", font = prompt).pack()
 e1 = tkinter.Entry(window)
-e1.grid(row = 0, column = 1, padx = 10, pady = 10)
+e1.pack(pady=5)
 
 #Handling word
 button1 = tkinter.Button(window, text = "Get definition", command = definition)
-button1.grid(row = 1, column = 0, sticky = tkinter.W, padx = 8)
+button1.pack(pady=2)
 
 #Displaying word
-#TODO
-# -Use a textbox or something?
+text = scrolledtext.ScrolledText(window, undo = True, width = 50, height = 15)
+text.pack()
 
 window.mainloop()
 
