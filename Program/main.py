bdcb51c091dd0fa3b391cdc61e32007ca1e338a2
Hassaan Abbasi <54244274+HassaanAbbasi@users.noreply.github.com>
2020-01-02 14:33:55 -0500

    Adding cli files with fix on file path issue

---
+++ Program/main.py
@@ -0,0 +1,71 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("defs.json"))
+
+#Prints the defintions of a given word
+def definition(word):
+    word = word.lower()
+
+    #Case where the word is correctly spelled and in data
+    if (word in data):
+        allDefinition = data[word]
+        enum = enumerate(allDefinition,1)
+
+        print(word.title())
+        for definition in enum:
+            print(f"{definition[0]} - {definition[1]}")
+
+    #Case where the word is a proper noun (Manchester, Paris, etc.)
+    elif (word.title() in data):
+        allDefinition = data[word.title()]
+        enum = enumerate(allDefinition,1)
+
+        print(word.title())
+        for definition in enum:
+            print(f"{definition[0]} - {definition[1]}")
+
+    #Case where the user enters acronyms (NASA,UN, etc.)
+    elif (word.upper() in data):
+        allDefinition = data[word.upper()]
+        enum = enumerate(allDefinition,1)
+
+        print(word.title())
+        for definition in enum:
+            print(f"{definition[0]} - {definition[1]}")
+
+    #Case where the word is spelled incorrectly
+    else:
+        matches = get_close_matches(word,data)
+
+        #Given word does not exist (has no matches)
+        if (len(matches) == 0):
+            print("That word doesn't exist in our dictionary. Sorry!")
+
+        else:
+            for match in matches:
+                selection = input(f"Did you mean {match}? Please input either 'Yes' or 'No': ")
+                while (selection != "Yes" and selection != "No"):
+                    selection = input("Please input either 'Yes' or 'No': ")
+
+                if (selection == "Yes"):
+                    allDefinition = data[match]
+                    enum = enumerate(allDefinition,1)
+
+                    print(match.title())
+                    for definition in enum:
+                        print(f"{definition[0]} - {definition[1]}")
+                    break
+                elif (selection == "No"):
+                    #Case where the last possible match has been reached and rejected
+                    if (matches.index(match) == (len(matches) - 1)):
+                        print("That word doesn't exist in our dictionary. Sorry!")
+                    else:
+                        continue
+
+word = input("Welcome to Diqtionary!\nEnter 'Q' if you want to close the program.\nEnter a word please: ") 
+while (word != "Q"):
+    definition(word)
+    word = input("\nEnter 'Q' if you want to close the program.\nEnter a word please: ")
+
+print("\nThanks for using Diqtionary!")
---
+++ README.md
@@ -0,0 +1,7 @@
+# Diqtionary
+Diqtionary (a combination of "Dictionary" and "IQ") is a program that returns all the definitions of a given word. However, it also has some smart built-in features:
+
+* Provides possible words you might have meant if you input a word incorrectly
+* Recognizes acronyms
+
+To run Diqtionary, download all the files and run the "main.py" file in the "Program" directory.
